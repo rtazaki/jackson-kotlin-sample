0e297577d2f3f9a2eb460d86d491624fb46280c9
rtazaki <rtazaki@gmail.com>
2018-06-02 17:31:08 +0900

    シリアライズもうまくいった。

--- README.md
+++ README.md
@@ -13,10 +13,17 @@ StringArrayがKotlinにないので、Array<String>を使う。(test2)
 ## 値の更新
 配列を更新しようとすると、
 Kotlin: Unsupported [Collection literals outside of annotations]が出てしまう?
+→doubleArrayOfが使える。(test1)、
+stringArrayOfとかanyArrayOfは存在しないのようなので、arrayOfで渡せばよい。(test2, test3)
 
 ## シリアライズ & ファイル書き込み
 mapper.writeValue(ClassLoader.getSystemResource("Test.json"), test)
-みたいにやったがうまくいかなかった。
+みたいにやったがうまくいかなかったのは、resourcesフォルダだから。
+(src/dist以下に置いた場合、File()をかませば、なんの問題もない。)
+### SerializationFeature.INDENT_OUTPUTの挙動について
+勝手にCRLFにしている? あと、IntelliJ IDEAのコード自動フォーマット(Ctrl+Alt+L)より、改行少な目。
 
 ## その他
 readで使ったファイルを上書きしたい。どうすればよいのか?
+→resourcesフォルダではなく、distフォルダなどを使ったほうがよさげ。
+(src/main/resourcesから読みだして、build/resourcesフォルダに書くから。)
--- src/main/kotlin/test/Main.kt
+++ src/main/kotlin/test/Main.kt
@@ -1,7 +1,9 @@
 package test
 
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import java.io.File
 
 data class Test(val first: TestNode, val second: TestNode, val third: TestNode)
 data class TestNode(var oldpara: Double, var oldpara2: Double, var newpara: DoubleArray)
@@ -15,20 +17,21 @@ data class TestNode3(var oldpara: Any, var oldpara2: Any, var newpara: Array<Any
 fun main(args: Array<String>) {
     println("Hello")
     val mapper = jacksonObjectMapper()
-    val test = mapper.readValue<Test>(ClassLoader.getSystemResource("Test.json"))
-    val test2 = mapper.readValue<Test2>(ClassLoader.getSystemResource("Test2.json"))
-    val test3 = mapper.readValue<Test3>(ClassLoader.getSystemResource("Test3.json"))
+    val test = mapper.readValue<Test>(File("src/dist/Test.json"))
+    val test2 = mapper.readValue<Test2>(File("src/dist/Test2.json"))
+    val test3 = mapper.readValue<Test3>(File("src/dist/Test3.json"))
     println(test)
     println(test2)
     println(test3)
-//    test.first.oldpara = 1.2
-//    test2.first.oldpara2 = "hoge"
-//    test3.n1.oldpara = 3.14
-//    test3.n4.oldpara2 = "aaaaaaaa"
-//    mapper.writerWithDefaultPrettyPrinter().writeValueAsString(test)
-//    mapper.writerWithDefaultPrettyPrinter().writeValueAsString(test2)
-//    mapper.writerWithDefaultPrettyPrinter().writeValueAsString(test3)
-//    mapper.writeValue(ClassLoader.getSystemResource("Test.json"), test)
-//    test.first.newpara = [1.2, 2.3]
-//    test3.n1.newpara = [9.8, 4.2, 3.14]
+    test.first.oldpara = 1.2
+    test.first.newpara = doubleArrayOf(1.2, 2.4)
+    test2.first.oldpara2 = "hoge"
+    test2.first.newpara = arrayOf("AAA", "BBB")
+    test3.n1.oldpara = 3.14
+    test3.n4.oldpara2 = "aaaaaaaa"
+    test3.n1.newpara = arrayOf(1.8, 4.2, 3.14)
+    mapper.enable(SerializationFeature.INDENT_OUTPUT)
+    mapper.writeValue(File("src/dist/Test.json"), test)
+    mapper.writeValue(File("src/dist/Test2.json"), test2)
+    mapper.writeValue(File("src/dist/Test3.json"), test3)
 }
